c864b799a492ea7fa44718202fa90cd10633fc5c
fyntasmart <fyntasmart@gmail.com>
2026-08-22 20:37:15 +0300

    Implement routing with BrowserRouter and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import Checkout from './Checkout.tsx'; // .tsx lagana zaroori hai
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Checkout from './Checkout.tsx';
+import Admin from './Admin.tsx';
 
 function App() {
   return (
-    <div>
-      <Checkout />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Customer ka page (Home) */}
+        <Route path="/" element={<Checkout />} />
+        
+        {/* Admin ka page */}
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
